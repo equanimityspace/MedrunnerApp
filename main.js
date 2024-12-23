31c599f01c3f5217214e8fbd0669497a265d8fe5
Ryan <ryfly2016@gmail.com>
2024-12-23 16:04:08 -0700

    security, css, resizing window. I need to remember to commit more

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, session, webContents } = require('electron')
+const { app, BrowserWindow, session, webContents, ipcMain, WebContentsView, dialog } = require('electron')
 const DiscordRPC = require('discord-rpc')
 const path = require('path')
 const { URL } = require('url')
@@ -11,19 +11,48 @@ const clientId = process.env.CLIENT_ID
 // Added security -- prevents app from interacting with other applications or "sensitive" system resources
 app.enableSandbox()
 
-const rpc = new DiscordRPC.Client({
-    transport: 'ipc'
-})
+let mainWindow
+let medrunnerButton
+let medTools
 
 function createWindow () {
-    const win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
+        show: false,
         width: 1600,
         height: 1000,
     webPreferences: {
-        preload: path.join(app.getAppPath(), 'preload.js')
+        preload: path.join(app.getAppPath(), 'preload.js'),
+        contextIsolation: true,
+        nodeIntegration: false,
+        nodeIntegrationInWorker: false
     }
     })
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
 
+    // Load med-tools first,
+    medTools = new WebContentsView()
+    mainWindow.contentView.addChildView(medTools)
+    medTools.webContents.loadURL('https://med-tools.space')
+    medTools.setBounds({x: 0, y: 40, width: 1600, height: 960})
+
+    // then load medrunner staff portal as start page
+    medrunnerView = new WebContentsView()
+    mainWindow.contentView.addChildView(medrunnerView)
+    medrunnerView.webContents.loadURL('https://staff.medrunner.space')
+    medrunnerView.setBounds({x: 0, y: 40, width: 1600, height: 960})
+
+    mainWindow.on('resize', () => {
+        const [newWidth, newHeight] = mainWindow.getSize()
+        const viewHeight = newHeight - 40
+      
+        // Update bounds for both views
+        medrunnerView.setBounds({ x: 0, y: 40, width: newWidth, height: viewHeight })
+        medTools.setBounds({ x: 0, y: 40, width: newWidth, height: viewHeight })
+    })
+
+    // set discord rich presence
     rpc.on('ready', () => {
         setActivity()
 
@@ -31,9 +60,23 @@ function createWindow () {
             setActivity()
         }, 15e3)
     })
-
-    win.loadURL('https://staff.medrunner.space')
 };
+
+ipcMain.on('show-view', (event, viewID) => {
+    if (viewID === 1) {
+        mainWindow.contentView.addChildView(medrunnerView)
+    } else if (viewID === 2) {
+        mainWindow.contentView.addChildView(medTools)
+    }
+})
+
+const rpc = new DiscordRPC.Client({
+    transport: 'ipc'
+})
+
+rpc.login({
+    clientId: clientId
+})
 
 async function setActivity() {
     rpc.setActivity({
@@ -48,10 +91,6 @@ async function setActivity() {
     ]
     });
 }
-
-rpc.login({
-    clientId: clientId
-})
 
 app.whenReady().then(() => {
     // only enable notifications if final URL matches medrunner staff portal
@@ -70,7 +109,10 @@ app.whenReady().then(() => {
     });
     
     // lift off!!
-    createWindow()    
+    createWindow()
+    
+    // load custom html
+    mainWindow.loadFile('assets/index.html')
 })
 
 // includes support for mac, just in case...
--- preload.js
+++ preload.js
@@ -2,10 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
     setTitle: (title) => ipcRenderer.send('set-title', title),
-    webPreferences: {
-        preload: path.join(app.getAppPath(), 'preload.js'),
-        contextIsolation: true,
-        nodeIntegration: false,
-        nodeIntegrationInWorker: false
-    }
+    showView: (viewID) => ipcRenderer.send('show-view', viewID),
 })
